bf8049d79a8d2b6bea631cce3cc82e9076f85336
ummetarin <ummehomairatarin@gmail.com>
2024-11-30 00:28:17 +0600

    Condition

--- Python.py
+++ Python.py
@@ -37,25 +37,40 @@
 # print(x)
 
 # string
-name="homaira apu"
-stri="allalalal"
-age=12
-for i in name:
-    print(i)
+# name="homaira apu"
+# stri="allalalal"
+# age=12
+# for i in name:
+#     print(i)
 
-print(name[::-1])
-print(name[:4])
-print(len(name))
-print(name.upper())
-print(name.replace('h','p'))
-print(name.strip())
-print(name.split(","))
+# print(name[::-1])
+# print(name[:4])
+# print(len(name))
+# print(name.upper())
+# print(name.replace('h','p'))
+# print(name.strip())
+# print(name.split(","))
 
-for i in range(len(name) - 1, -1, -1):
-    print(name[i])
+# for i in range(len(name) - 1, -1, -1):
+#     print(name[i])
     
-print(name+stri)
-print("the book is "+name+"is mine")
-print("homaira age is"+str(age))
-print(f"homaira is {age}")
+# print(name+stri)
+# print("the book is "+name+"is mine")
+# print("homaira age is"+str(age))
+# print(f"homaira is {age}")
 
+# condition
+x=int(input())
+if(x%2==0):
+    print("even")
+else:
+    print("odd")
+
+if(x<=100 and x>=80):
+    print("a+")
+elif(x<=79 and x>=75):
+    print("a")
+elif(x<=74 and x>=70):
+    print("a-")
+else:
+    print("fail")
